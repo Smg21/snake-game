d6d34ee34408cb5f9dbb2315d826a2012d9d4b16
Sabrina <Smg21022@gmail.com>
2024-01-21 17:37:14 -0700

    made some basic functions

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("test");
+//Define HTML elements that we need 
+const board = document.getElementById('game-board');
+
+//define game variables
+let snake = [{x: 10, y: 10}]; //This is where the snake starts
+
+//draw game map, snake, food
+function draw(){
+    board.innerHTML = '';
+    drawSnake();
+}
+
+//draw snake, define how snake looks
+function drawSnake(){
+snake.forEach((segment) => {
+    const snakeElement = createGameElement('div', 'snake');
+}) //This is responsible for giving it snake elements 
+}
+
+//Create a snake or food cube/div
+function createGameElement(tag, className) {
+    const element = document.createElement(tag); //creating a new element using the same way to target the html but instead its creating a new html element within javascript
+    element.className = className; 
+}
